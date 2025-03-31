c6d42eddfb14026358cc47a0259356a44b6d6393
Saikrishna <saikrishnaguntoju@gmail.com>
2025-03-31 20:35:38 +0530

    test

--- Assets/Scripts/KartAgent.cs
+++ Assets/Scripts/KartAgent.cs
@@ -28,19 +28,29 @@ public class KartAgent : Agent
       //Collecting extra Information that isn't picked up by the RaycastSensors
       public override void CollectObservations(VectorSensor sensor)
       {
-         
+         Vector3 diff = _checkpointManager.nextCheckPointToReach.transform.position - transform.position;
+
+         sensor.AddObservation(diff/20f); // Normalizing the distance to the next checkpoint         
+
+         AddReward(-0.001f);
       }
 
       //Processing the actions received
       public override void OnActionReceived(ActionBuffers actions)
       {
-         
+         var input = actions.ContinuousActions;
+
+         _kartController.Steer(input[0]); // Left/Right
+         _kartController.ApplyAcceleration(input[1]); // Accelerate
       }
       
       //For manual testing with human input, the actionsOut defined here will be sent to OnActionRecieved
       public override void Heuristic(in ActionBuffers actionsOut)
       {
-         
+         var action = actionsOut.ContinuousActions;
+
+         action[0] = Input.GetAxis("Horizontal"); // Left/Right
+         action[1] = Input.GetKey(KeyCode.W) ? 1f : 0f; // Accelerate
       }
       
    #endregion
